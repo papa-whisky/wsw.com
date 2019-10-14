900eea378f7ee1aff3ed12746c894e3e77f67b92
Paul Williams <paul@pdwilliams.net>
2019-10-14 23:20:16 +1100

    Shrink logo when menu clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 const button = document.querySelector("button");
+const img = document.querySelector("img");
 
 button.onclick = () => {
   button.classList.toggle("is-active");
+  img.classList.toggle("shrink");
 };
