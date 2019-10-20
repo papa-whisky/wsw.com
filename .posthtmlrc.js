36693f0a5f60de7ea360f65d585f92e19684bd3d
Paul Williams <paul@pdwilliams.net>
2019-10-20 23:36:38 +1100

    Add favicon

---
+++ .posthtmlrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    "posthtml-favicons": {
+      configuration: {
+        appName: "Clare + Paul",
+        appShortName: "WSW",
+        appDescription: "Williams Sheldon-Williams Wedding",
+        developerName: "Paul Williams",
+        developerURL: "https://github.com/papa-whisky",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          windows: true,
+          yandex: false
+        }
+      }
+    }
+  }
+};
