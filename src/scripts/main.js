938da1d0fbd18677482f7169020dbd0612607cd7
Paul Williams <paul@pdwilliams.net>
2019-10-20 16:14:41 +1100

    Add menu items

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 const button = document.querySelector("button");
 const img = document.querySelector("img");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector("#menu");
 
 button.onclick = () => {
   button.classList.toggle("is-active");
-  img.classList.toggle("shrink");
+  // img.classList.toggle("shrink");
   menu.classList.toggle("visible");
 };
