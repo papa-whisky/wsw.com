06e4e244f29f020936dd39511c92798691355d60
Paul Williams <paul@pdwilliams.net>
2019-10-20 15:36:21 +1100

    Add content sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 const button = document.querySelector("button");
 const img = document.querySelector("img");
+const menu = document.querySelector(".menu");
 
 button.onclick = () => {
   button.classList.toggle("is-active");
   img.classList.toggle("shrink");
+  menu.classList.toggle("visible");
 };
