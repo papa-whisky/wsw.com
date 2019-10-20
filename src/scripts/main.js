a78d9cef1856be8c9a9b68c22a33e5d5e1f5f99a
Paul Williams <paul@pdwilliams.net>
2019-10-20 17:45:59 +1100

    Form WIP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
 const button = document.querySelector("button");
 const img = document.querySelector("img");
-const menu = document.querySelector("#menu");
+const menu = document.querySelector("#form");
 
 button.onclick = () => {
   button.classList.toggle("is-active");
-  // img.classList.toggle("shrink");
+  img.classList.toggle("shrink");
   menu.classList.toggle("visible");
+};
+
+const form = document.querySelector("form");
+const submit = document.querySelector('button[type="submit"]');
+
+submit.onclick = e => {
+  e.preventDefault();
+  const data = new URLSearchParams(new FormData(form));
+  fetch(form.action, { body: data, method: "post" });
 };
