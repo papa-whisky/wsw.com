9a509aafa40dfdc0da2174d099a7c3d97d0dd4ed
Paul Williams <paul@pdwilliams.net>
2019-10-20 19:17:09 +1100

    Menu flow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,35 @@
-const button = document.querySelector("button");
-const img = document.querySelector("img");
-const menu = document.querySelector("#form");
+const menuBtn = document.querySelector(".hamburger");
+const titleImg = document.querySelector(".title");
+const menuSection = document.querySelector("#menu");
+const formSection = document.querySelector("#form");
+const detailsSection = document.querySelector("#details");
 
-button.onclick = () => {
-  button.classList.toggle("is-active");
-  img.classList.toggle("shrink");
-  menu.classList.toggle("visible");
+menuBtn.onclick = () => {
+  if (menuBtn.classList.contains("is-active")) {
+    menuBtn.classList.remove("is-active");
+    titleImg.classList.remove("shrink");
+    menuSection.classList.remove("visible");
+    formSection.classList.remove("visible");
+    detailsSection.classList.remove("visible");
+  } else {
+    menuBtn.classList.add("is-active");
+    menuSection.classList.add("visible");
+  }
+};
+
+const detailsBtn = document.querySelector(".details-button");
+const formBtn = document.querySelector(".form-button");
+
+detailsBtn.onclick = () => {
+  titleImg.classList.add("shrink");
+  menuSection.classList.remove("visible");
+  detailsSection.classList.add("visible");
+};
+
+formBtn.onclick = () => {
+  titleImg.classList.add("shrink");
+  menuSection.classList.remove("visible");
+  formSection.classList.add("visible");
 };
 
 const form = document.querySelector("form");
