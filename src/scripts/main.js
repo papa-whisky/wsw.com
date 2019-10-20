22335f6ffde13d38746481c29b14320c5f70224b
Paul Williams <paul@pdwilliams.net>
2019-10-20 18:53:13 +1100

    Add form success/failure messages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,5 +14,13 @@ const submit = document.querySelector('button[type="submit"]');
 submit.onclick = e => {
   e.preventDefault();
   const data = new URLSearchParams(new FormData(form));
-  fetch(form.action, { body: data, method: "post" });
+  fetch(form.action, { body: data, method: "post" }).then(response => {
+    if (!response.ok) {
+      document.querySelector(".form-wrapper").style.display = "none";
+      document.querySelector(".form-fail").style.display = "block";
+    } else {
+      document.querySelector(".form-wrapper").style.display = "none";
+      document.querySelector(".form-success").style.display = "block";
+    }
+  });
 };
